c8f8240c1cef91531e2ec401f72f25928d00430a
Heysland Centeno <heysbor@gmail.com>
2026-08-30 22:18:10 -0600

    Parte 1

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class Form1
+Partial Class frmBlocNotas
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,8 +22,19 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
         Label1 = New Label()
         Label2 = New Label()
+        mnuPrincipal = New MenuStrip()
+        tsPrincipal = New ToolStrip()
+        rtbDocumento = New RichTextBox()
+        cmsTexto = New ContextMenuStrip(components)
+        stsInferior = New StatusStrip()
+        dlgAbrir = New OpenFileDialog()
+        dlgGuardar = New SaveFileDialog()
+        dlgFuente = New FontDialog()
+        dlgColor = New ColorDialog()
+        tmrReloj = New Timer(components)
         SuspendLayout()
         ' 
         ' Label1
@@ -31,33 +42,111 @@ Partial Class Form1
         Label1.AutoSize = True
         Label1.Location = New Point(311, 39)
         Label1.Name = "Label1"
-        Label1.Size = New Size(53, 15)
+        Label1.Size = New Size(0, 15)
         Label1.TabIndex = 0
-        Label1.Text = "Pico mal"
         ' 
         ' Label2
         ' 
         Label2.AutoSize = True
         Label2.Location = New Point(265, 61)
         Label2.Name = "Label2"
-        Label2.Size = New Size(44, 15)
+        Label2.Size = New Size(0, 15)
         Label2.TabIndex = 1
-        Label2.Text = "7 tapas"
         ' 
-        ' Form1
+        ' mnuPrincipal
+        ' 
+        mnuPrincipal.Location = New Point(0, 0)
+        mnuPrincipal.Name = "mnuPrincipal"
+        mnuPrincipal.Size = New Size(800, 24)
+        mnuPrincipal.TabIndex = 2
+        mnuPrincipal.Text = "MenuStrip1"
+        ' 
+        ' tsPrincipal
+        ' 
+        tsPrincipal.GripStyle = ToolStripGripStyle.Hidden
+        tsPrincipal.Location = New Point(0, 24)
+        tsPrincipal.Name = "tsPrincipal"
+        tsPrincipal.Size = New Size(800, 25)
+        tsPrincipal.TabIndex = 3
+        tsPrincipal.Text = "ToolStrip1"
+        ' 
+        ' rtbDocumento
+        ' 
+        rtbDocumento.ContextMenuStrip = cmsTexto
+        rtbDocumento.Dock = DockStyle.Fill
+        rtbDocumento.Font = New Font("Consolas", 11.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        rtbDocumento.Location = New Point(0, 49)
+        rtbDocumento.Name = "rtbDocumento"
+        rtbDocumento.Size = New Size(800, 401)
+        rtbDocumento.TabIndex = 4
+        rtbDocumento.Text = ""
+        ' 
+        ' cmsTexto
+        ' 
+        cmsTexto.Name = "cmsTexto"
+        cmsTexto.Size = New Size(61, 4)
+        ' 
+        ' stsInferior
+        ' 
+        stsInferior.Location = New Point(0, 428)
+        stsInferior.Name = "stsInferior"
+        stsInferior.Size = New Size(800, 22)
+        stsInferior.TabIndex = 6
+        stsInferior.Text = "StatusStrip1"
+        ' 
+        ' dlgAbrir
+        ' 
+        dlgAbrir.FileName = "OpenFileDialog1"
+        dlgAbrir.Filter = "Archivos de texto (*.txt)|*.txt|Todos (*.*)|*.*"
+        ' 
+        ' dlgGuardar
+        ' 
+        dlgGuardar.DefaultExt = "txt"
+        ' 
+        ' dlgFuente
+        ' 
+        dlgFuente.ShowColor = True
+        ' 
+        ' dlgColor
+        ' 
+        dlgColor.FullOpen = True
+        ' 
+        ' tmrReloj
+        ' 
+        tmrReloj.Enabled = True
+        tmrReloj.Interval = 1000
+        ' 
+        ' frmBlocNotas
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(stsInferior)
+        Controls.Add(rtbDocumento)
+        Controls.Add(tsPrincipal)
         Controls.Add(Label2)
         Controls.Add(Label1)
-        Name = "Form1"
-        Text = "Form1"
+        Controls.Add(mnuPrincipal)
+        MainMenuStrip = mnuPrincipal
+        MinimumSize = New Size(600, 400)
+        Name = "frmBlocNotas"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Bloc de Notas VB.NET"
         ResumeLayout(False)
         PerformLayout()
     End Sub
 
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
+    Friend WithEvents mnuPrincipal As MenuStrip
+    Friend WithEvents tsPrincipal As ToolStrip
+    Friend WithEvents rtbDocumento As RichTextBox
+    Friend WithEvents cmsTexto As ContextMenuStrip
+    Friend WithEvents stsInferior As StatusStrip
+    Friend WithEvents dlgAbrir As OpenFileDialog
+    Friend WithEvents dlgGuardar As SaveFileDialog
+    Friend WithEvents dlgFuente As FontDialog
+    Friend WithEvents dlgColor As ColorDialog
+    Friend WithEvents tmrReloj As Timer
 
 End Class
